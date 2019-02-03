e23c6bc79d153d23ba8f12f413e3037b9ce76342
Andrew Wick <a.wick17@gmail.com>
2019-02-03 12:04:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,23 +7,3 @@ def position_taken?(board, index)
     taken = true
 end
 
-
-
-
-
-#def position_taken?(board, index)
-  # basic solution:
-
-  # taken = nil
-  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-  #   taken = false
-  # else
-  #   taken = true
-  # end
-  # taken
-
-  # advanced solution w/ ternary operator
-  #(board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-    #false : true
-
-#end
